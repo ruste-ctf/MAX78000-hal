27201c27b69804936d8d64f9d9fb1c13113d2a8f
corigan01 <corigan01@gmail.com>
2024-02-04 21:27:58 -0600

    Add Const Assert to lib.rs
    Include `const_assert` to help prevent const values that are not
    within range for register types.

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,13 @@
 pub mod error;
 pub mod i2c;
 pub mod memory_map;
+
+/// # Const Assert
+/// Assert in a const context, useful for making sure that
+/// provided constants fall in expected range.
+#[macro_export]
+macro_rules! const_assert {
+    ($($tt:tt)*) => {
+        const _: () = assert!($($tt)*);
+    };
+}
