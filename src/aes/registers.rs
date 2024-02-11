60cbcfca67718ad2bd1e9823a1a81b2ba189478c
Andrew Spellman <spellmanandrew@protonmail.com>
2024-02-10 19:33:39 -0600

    AES: Added Control Register

--- src/aes/registers.rs
+++ src/aes/registers.rs
@@ -18,3 +18,52 @@ mod rro {
     /// # AES Data FIFO Register
     pub const AES_FIFO: usize = 0x0010;
 }
+
+/// # AES Control Register
+/// The AES control register. See Page 360-361, Table 24-4.
+pub struct ControlRegister<const PORT_PTR: usize> {}
+reg_impl!(RW, ControlRegister, rro::AES_CTRL);
+
+impl<const PORT_PTR: usize> ControlRegister<PORT_PTR> {
+    bit_impl! {8..=9, RW u8,
+    /// # Set Encryption Type
+    set_encryption_type,
+    /// # Get Encryption Type
+    get_encryption_type}
+
+    bit_impl! {6..=7, RW u8,
+    /// # Set Encryption Key Size
+    set_encryption_key_size,
+    /// # Get Encryption Key Size
+    get_encryption_key_size}
+
+    bit_impl! {5, RESET, // FIXME This needs to be renamed / changed
+    /// # Activate Flush Data Output FIFO
+    activate_flush_data_output_fifo}
+
+    bit_impl! {4, RESET, // FIXME This needs to be renamed / changed
+    /// # Activate Flush Data Input FIFO
+    activate_flush_data_input_fifo}
+
+    bit_impl! {3, RESET, // FIXME This needs to be renamed / changed
+    /// # Activate Start AES Calculation
+    activate_start_aes_calculation}
+
+    bit_impl! {2, RW,
+    /// # Set DMA Request To Write Data Input FIFO
+    set_dma_request_to_write_data_input_fifo,
+    /// # Get DMA Request To Write Data Input FIFO
+    get_dma_request_to_write_data_input_fifo}
+
+    bit_impl! {1, RW,
+    /// # Set DMA Request To Read Data Output FIFO
+    set_dma_request_to_read_data_output_fifo,
+    /// # Get DMA Request To Read Data Output FIFO
+    get_dma_request_to_read_data_output_fifo}
+
+    bit_impl! {0, RW,
+    /// # Set AES Enable
+    set_aes_enable,
+    /// # Get AES Enable
+    get_aes_enable}
+}
