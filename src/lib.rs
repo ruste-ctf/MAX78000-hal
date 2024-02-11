d93d15aaa56f1c577f41d30df817e8eb47671ebf
corigan01 <corigan01@gmail.com>
2024-02-11 15:58:29 -0600

    Lib: Include Peripheral clock to the lib.rs file
    Include Peripheral clock access that will calculate
    the clock the same way the msdk does internally

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,12 @@ extern "C" {
     pub(crate) static SYSTEM_CORE_CLOCK: u32;
 }
 
+/// # Core Peripheral Clock
+/// Get the peripheral clock used for timing things like I2C and UART for the CPU.
+pub(crate) fn core_peripheral_clock() -> u32 {
+    unsafe { SYSTEM_CORE_CLOCK / 2 }
+}
+
 /// # Const Assert
 /// Assert in a const context, useful for making sure that
 /// provided constants fall in expected range.
