e534c0a175084a07dc9e8af91fb284428c46d95d
Andrew Spellman <spellmanandrew@protonmail.com>
2024-02-15 21:37:19 -0600

    TRNG: Fixed Safety and Interal Key State

--- src/trng/mod.rs
+++ src/trng/mod.rs
@@ -4,7 +4,7 @@ use registers::Registers;
 
 pub struct TRNG {
     registers: Registers,
-    pub has_key: bool,
+    has_key: bool,
 }
 
 impl TRNG {
@@ -13,28 +13,40 @@ impl TRNG {
             registers: Registers::new(mmio::TRNG),
             has_key: false,
         };
-        unsafe {
-            registers.registers.set_generate_key(true);
-        }
-        registers.has_key = true;
-        return registers;
+        registers.generate_new_key();
+        registers
+    }
+
+    pub fn has_key(&self) -> bool {
+        self.has_key
     }
 
     pub fn wipe_key(&mut self) {
         unsafe {
             self.registers.set_wipe_key(true);
         }
+        self.has_key = false;
     }
 
     pub fn generate_new_key(&mut self) {
         unsafe {
             self.registers.set_generate_key(true);
         }
+        self.has_key = true;
     }
 
-    pub fn get_trng_data(&self) -> u32 {
+    pub fn get_trng_data(&mut self) -> u32 {
         // FIXME use interrupts
-        while self.registers.get_random_number_ready() == false {}
+        assert!(self.has_key);
+        while !self.registers.get_random_number_ready() {}
+        self.registers.get_trng_data()
+    }
+
+    /// # Safety
+    /// This function is for educational purposes only. It should only be used to
+    /// learn about what happens when you generate a number without having a key.
+    pub unsafe fn unchecked_get_trng_data(&self) -> u32 {
+        while !self.registers.get_random_number_ready() {}
         self.registers.get_trng_data()
     }
 }
